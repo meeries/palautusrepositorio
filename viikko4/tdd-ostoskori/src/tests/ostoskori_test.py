de8d4a820d44fe1a0eecbe4041691127e3cd2eac
Meeri <meeri.simpanen@helsinki.fi>
2022-11-24 10:02:50 +0100

    5

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -19,3 +19,10 @@ class TestOstoskori(unittest.TestCase):
         kakku = Tuote("Kakku", 5)
         self.kori.lisaa_tuote(kakku)
         self.assertEqual(self.kori.hinta(), 5)
+
+    def test_kahden_erituotteen_jalkeen_hinta_oikein(self):
+        kakku = Tuote("Kakku", 5)
+        omena = Tuote("Omena", 5)
+        self.kori.lisaa_tuote(kakku)
+        self.kori.lisaa_tuote(omena)
+        self.assertEqual(self.kori.hinta(), 10)
