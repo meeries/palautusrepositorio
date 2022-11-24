f2c96c683b6b7922f1be6998b949f0041f77c1bd
Meeri <meeri.simpanen@helsinki.fi>
2022-11-24 10:01:19 +0100

    2 ja 3

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,22 +3,27 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        pass
+        self.kori = {}
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        return 0
-        # kertoo korissa olevien tavaroiden lukumäärän
-        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
-        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
+        maara = 0
+        for tuote in self.kori.values():
+            maara += tuote.lukumaara()
+
+        return maara
 
     def hinta(self):
-        return 0
-        # kertoo korissa olevien ostosten yhteenlasketun hinnan
+        hinta = 0
+        for tuote in self.kori.values():
+            hinta += tuote.hinta()
+        return hinta
 
     def lisaa_tuote(self, lisattava: Tuote):
-        # lisää tuotteen
-        pass
+        if lisattava.nimi in self.kori:
+            self.kori[lisattava.nimi].muuta_lukumaaraaa(1)
+        else:
+            self.kori[lisattava.nimi] = Ostos(lisattava)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -9,3 +9,13 @@ class TestOstoskori(unittest.TestCase):
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
+
+    def test_korissa_yksi_tavara_yhden_lisaamisen_jalkeen(self):
+        kakku = Tuote("Kakku", 5)
+        self.kori.lisaa_tuote(kakku)
+        self.assertEqual(self.kori.tavaroita_korissa(), 1)
+
+    def test_yhden_tuotteen_jalkeen_hinta_oikein(self):
+        kakku = Tuote("Kakku", 5)
+        self.kori.lisaa_tuote(kakku)
+        self.assertEqual(self.kori.hinta(), 5)
